90e5361b87b0a5cb375998f7fc13def8bf758c2e
jykim1214 <kjyanylove@gmail.com>
2017-05-24 19:21:05 +0900

    Movie Ticketing

--- src/Employee.java
+++ src/Employee.java
@@ -2,15 +2,15 @@
 public class Employee {
 
 	public TicketMachine ticketMachine;
-	
+
 	public Employee() {
 		ticketMachine = new TicketMachine();
-		
+
 	}
 
 	public void reservation() {
 		System.out.println("Employee : TicketMachine에게 A영화의 영화 정보를 물어봅니다.");
 		ticketMachine.movieInfo();
 	}
-	
+
 }
