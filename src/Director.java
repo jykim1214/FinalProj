0ee0751a4915870c1556da552f4b9fc9effd3708
jykim1214 <kjyanylove@gmail.com>
2017-05-24 19:41:20 +0900

    Movie Ticketing

--- src/Director.java
+++ src/Director.java
@@ -11,10 +11,10 @@ public class Director {
 		System.out.println("Director : Customer를 생성합니다.");
 		String customerName = "김진영";
 		Customer customer = new Customer(customerName);
-		
+
 		System.out.println("Director : Employee를 생성합니다.");
 		Employee employee = new Employee();
-		
+
 		System.out.println("Director : Customer에게 영화를 예매하도록 지시합니다.");
 		customer.reserveMovie(employee);
 	}
